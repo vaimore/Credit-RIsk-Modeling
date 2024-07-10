9caa1b50f70eeadf84d34b847bd48d2d8e222508
vaimore <37243872+vaimore@users.noreply.github.com>
2024-07-10 20:50:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ if st.button("Predict"):
     EAD = loan_amnt
     EL = probability * EAD * LGD
     st.write(f"Exposure at Default (EAD): {EAD}")
-    st.write(f"Expected Loss (EL): {EL:.,2f}")
+    st.write(f"Expected Loss (EL): {EL:,.0f}")
